57c7f0bcb05fffcfde56167f83e8d704e0423b2b
PedroTeixeiraa <pedroteixeiraalves007@gmail.com>
2023-07-12 22:33:03 -0300

    fix: update app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teste pipeline!
+          Teste pipeline 100%!
         </p>
         <a
           className="App-link"
